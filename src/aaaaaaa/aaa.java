d103820c34a165e16f9c5621187ec0cd67de9fa0
user <user@EB04>
2020-09-29 09:03:01 +0800

    map變成 apple

--- src/aaaaaaa/aaa.java
+++ src/aaaaaaa/aaa.java
@@ -4,7 +4,7 @@ public class aaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("that is a map");
+		System.out.println("that is an apple");
 	}
 
 }
